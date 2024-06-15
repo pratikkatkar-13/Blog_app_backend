f3d2353d7ef6c84e5b9eed3d916fb70364bc04c0
OIBSIP <pratikkatkar13@gmail.com>
2024-06-15 11:52:14 +0530

    files addedd

---
+++ controllers/commentController.js
@@ -0,0 +1,33 @@
+//import model
+const Post = require("../models/postModel");
+const Comment = require("../models/commentModel");
+
+//business logic 
+exports.createComment = async (req, res) => {
+    try{
+        //fetch data from req body 
+        const {post, user, body} = req.body;
+        //create a comment object
+        const comment = new Comment({
+            post,user,body
+        });
+
+        //save the new comment into the database
+        const savedComment = await comment.save();
+
+        //find the post by ID, add the new commnet to its comments array
+        const udpatedPost = await Post.findByIdAndUpdate(post, {$push: {comments: savedComment._id} }, {new: true}  )
+                            .populate("comments") //populate the comments array with comment documents thst is printing all the documents intstead of onlu comment document id 
+                            .exec(); // whole syntax
+
+        res.json({
+            post: udpatedPost,
+        });
+
+    }
+    catch(error) {
+        return res.status(500).json({
+            error: "Error While Creating comment" ,
+        });
+    }
+};
